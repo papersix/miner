a4730ea0fda4c4642d3b6ec06154cf4cbe57473f
Rahul Alexander <alexander@chroma.org>
2016-06-09 21:43:31 -0400

    image added

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
 var $button = $('button');
 var $input = $('input');
 var $rebus = $('img');
+var $h1 = $('h1');
 var turn = 0
 var RebusArray =
-[
+ [
         {
         answer:"changing times",
         location: "../img/01.jpg"
@@ -13,10 +14,33 @@ var RebusArray =
         {
         answer:"not for all the tea in china",
         location: "../img/02.jpg"
-        }
+        },
+         {
+        answer:"it doesn't ring a bell",
+        location: "../img/03.jpg"
+        },
+         {
+        answer:"richard nixon",
+        location: "../img/04.jpg"
+        },
+         {
+        answer:"not for all the tea in china",
+        location: "../img/05.jpg"
+        },
+         {
+        answer:"take it or leave it",
+        location: "../img/06.jpg"
+        },
 ];
 
-var turner = function() {turn++}
+var turner = function() {
+  turn++;
+  if (turn %2 === 0) {
+    $h1.text('Player 1')
+  } else {
+    $h1.text('Player 2')
+  }
+}
 
 var randomNo = function () {
 
@@ -26,8 +50,9 @@ var randomNo = function () {
 $button.click(function(){
 
   var randomRebus = RebusArray[randomNo()];
-  turner()
+  turner();
 
+$rebus.attr("src", randomRebus.location);
 
   console.log(randomRebus.answer);
   console.log(randomRebus.location);
